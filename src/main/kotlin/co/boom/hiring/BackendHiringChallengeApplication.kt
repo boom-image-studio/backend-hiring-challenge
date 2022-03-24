b960c5a2978808aad564dac867b2bc29f2910b80
Di Stefano Luca <luca.distefano@boom.co>
2022-03-24 16:02:42 +0100

    created base kotlin project

---
+++ src/main/kotlin/co/boom/hiring/BackendHiringChallengeApplication.kt
@@ -0,0 +1,11 @@
+package co.boom.hiring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendHiringChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendHiringChallengeApplication>(*args)
+}
