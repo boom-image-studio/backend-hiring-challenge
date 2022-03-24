ed6fb878133931e9322dc47bec2a68c098714484
Di Stefano Luca <luca.distefano@boom.co>
2022-03-24 15:52:41 +0100

    Added base java project

---
+++ src/main/java/co/boom/hiring/BackendHiringChallengeApplication.java
@@ -0,0 +1,13 @@
+package co.boom.hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHiringChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHiringChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/boom/hiring/BackendHiringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.boom.hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHiringChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
